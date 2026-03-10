27479758e2f07f30aef4710e56e66d89fc2111d1
DobrevaI <iidobreva24a@gmail.com>
2026-03-10 14:20:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 st.title("Library")
 if "books" not in st.session_state:
-  st.session_state.books=[]
-  title=st.text_input("book title")
-  author=st.text_input("book author")
-  price=st.number_input("book price", min_value=0.0)
+  st.session_state.books = []
+  title = st.text_input("book title")
+  author = st.text_input("book author")
+  price = st.number_input("book price", min_value = 0.0)
   if st.button("Add the book"):
-    book={
+    book = {
       "title": title,
       "author": author,
       "price": price
@@ -14,7 +14,7 @@ if "books" not in st.session_state:
     st.session_state.book.append(book)
     st.success("The book has been added!")
     if st.button("Show all books"):
-      if len(st.session_state.books)==0:
+      if len(st.session_state.books) == 0:
         st.write("There aren't any added books")
       else:
         for book in st.session_state.books:
@@ -22,31 +22,31 @@ if "books" not in st.session_state:
           st.write("author", book["author"])
           st.write("price", book["price"])
           st.write("--------------------")
-          search_author=st.text_input("Enter the author's name")
+          search_author = st.text_input("Enter the author's name")
           if st.button("search author"):
-            found=False
+            found = False
             for book in st.session_state.books:
-              if book["author"]==search_author:
+              if book["author"] == search_author:
                 st.write(book)
-                found=True
-              if found==False:
+                found = True
+              if found == False:
                 st.write("The author isn't found")
-          search_title=st.text_input("Enter the book's title")
+          search_title = st.text_input("Enter the book's title")
           if st.button("search title"):
-            found=False
+            found = False
             for book in st.session_state.books:
-              if book["title"]==search_title:
+              if book["title"] == search_title:
                 st.write(book)
-                found=True
-              if found==False:
+                found = True
+              if found == False:
                 st.write("The title isn't found")
           if st.button("Show the cheapest book"):
-            if len(st.session_state.books)==0:
+            if len(st.session_state.books) == 0:
               st.write("There isn't a book")
             else:
-              cheapest=st.session_state.books[0]
+              cheapest = st.session_state.books[0]
               for book in st.session_state.books:
                 if book["price"]<cheapest["price"]:
-                  cheapest=book
+                  cheapest = book
                   st.write("The cheapest book is")
                   st.write(cheapest)
