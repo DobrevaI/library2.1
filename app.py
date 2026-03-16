5f117b57fde3e26b67cc85a0aa706a13929b82dc
DobrevaI <iidobreva24a@gmail.com>
2026-03-16 21:53:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ if st.button("Add the book"):
               st.write("There isn't a book")
             else:
               cheapest = st.session_state.books[0]
-              for book in st.session_state.books:
-      if book["price"]<cheapest["price"]:
-        cheapest = book
-        st.write("The cheapest book is")
-        st.write(cheapest)
+      for book in st.session_state.books:
+        if book["price"]<cheapest["price"]:
+          cheapest = book
+          st.write("The cheapest book is")
+          st.write(cheapest)
