92257f38d4ae8833c30f97ea65f4e3b51ea4bb74
DobrevaI <iidobreva24a@gmail.com>
2026-03-16 21:54:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ if "books" not in st.session_state:
   price = st.number_input("book price", min_value = 0.0)
 if st.button("Add the book"):
   book = {
-      "title": title,
-      "author": author,
-      "price": price
-      }
+    "title": title,
+    "author": author,
+    "price": price
+  }
   st.session_state.books.append(book)
   st.success("The book has been added!")
   if st.button("Show all books"):
